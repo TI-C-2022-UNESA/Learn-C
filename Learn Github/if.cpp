b3a119d133cff3d3b02aea87112c4f6fe853642d
Renaldyp <37020760+renaldypermana@users.noreply.github.com>
2022-09-07 19:53:54 +0700

    Tugas github

---
+++ Learn Github/if.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	string i;
+	cout<<"Halo ini program untuk belajar"<<endl;
+	cout<<"Masukkan Kalimat ";
+	cin>>i;
+	
+	if(i == "sugeng"){
+		cout<<"howak";
+	} else {
+		cout<<"tak bisa bahasa inggris";
+	}
+}
